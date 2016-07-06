115fbb4baaf9de02919af9b28143db48a63f42e9
jfbischoff <bischoff@vsp.tu-berlin.de>
2016-07-06 11:09:32 +0200

    AgentFactory

--- src/main/java/org/matsim/tutorial/class2016/bikesharing/BikesharingAgentFactory.java
+++ src/main/java/org/matsim/tutorial/class2016/bikesharing/BikesharingAgentFactory.java
@@ -1,17 +1,49 @@
 package org.matsim.tutorial.class2016.bikesharing;
 
+import javax.inject.Inject;
+
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.mobsim.framework.MobsimAgent;
 import org.matsim.core.mobsim.qsim.agents.AgentFactory;
+import org.matsim.core.mobsim.qsim.agents.PersonDriverAgentImpl;
+import org.matsim.core.mobsim.qsim.interfaces.Netsim;
 
 public class BikesharingAgentFactory implements AgentFactory {
 
 	//We need to determine: An agent going by car may become a "normal" DriverAgent, our bikesharers need to "become special" agents
 	
+	private final Netsim simulation;
+
+	@Inject
+	public BikesharingAgentFactory(final Netsim simulation) {
+		this.simulation = simulation;
+	}
+	
 	@Override
 	public MobsimAgent createMobsimAgentFromPerson(Person p) {
+		Plan plan = p.getSelectedPlan();
+		for (PlanElement pe : plan.getPlanElements()){
+			if (pe instanceof Leg){
+				Leg leg = (Leg) pe;
+				if (leg.getMode().equals("bike"))
+				{
+					createBikesharingAgent(p);
+				}
+					
+			}
+		}
+		
 		// TODO Auto-generated method stub
-		return null;
+		return 	new PersonDriverAgentImpl(p.getSelectedPlan(), this.simulation); 
+
+	}
+
+	private void createBikesharingAgent(Person p) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
